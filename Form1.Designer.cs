a3123e8702c5704e299c9c85e8861ac28fcb2e90
BigTuna25 <stephen.james.meyers@gmail.com>
2016-02-18 07:18:15 -0700

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ProjectNF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.closeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.Image = global::ProjectNF.Properties.Resources.Power_icon;
+            this.closeButton.Location = new System.Drawing.Point(559, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(81, 84);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProjectNF.Properties.Resources.jarvis_theme_by_zenoxen;
+            this.ClientSize = new System.Drawing.Size(641, 400);
+            this.Controls.Add(this.closeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace ProjectNF
+{
+    public partial class Form1 : Form
+    {
+        // Create new SpeechRecognizer instance.
+        SpeechRecognizer sr = new SpeechRecognizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialize_SpeechRecognition();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            sr.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized);
+        }
+
+        // SpeechRecognized Handler
+        void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Netflix":
+                    System.Diagnostics.Process.Start("http://www.netflix.com");
+                    break;
+                case "Steam":
+                    System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Steam\\Steam.exe");
+                    break;
+                case "Facebook":
+                    System.Diagnostics.Process.Start("http://www.facebook.com");
+                    break;
+                case "Go to Sleep":
+                    Application.Exit();
+                    break;
+            }
+        }
+
+        void initialize_SpeechRecognition()
+        {            
+            // Create a grammar to recognize choices.
+            Choices applicationChoices = new Choices();
+            applicationChoices.Add(new string[] { "Netflix", "Steam", "Facebook", "Go to Sleep" });
+
+            // Create a GrammarBuilder object and append the choices object.
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(applicationChoices);
+
+            // Create the grammar instance and load it into the speech recognizer.
+            Grammar g = new Grammar(gb);
+            sr.LoadGrammar(g);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
